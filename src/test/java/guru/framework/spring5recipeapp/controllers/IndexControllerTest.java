f4f58750da222580559d93f32d14a94b97d3e96f
Sienna Hu <xh244@cornell.edu>
2021-01-13 12:11:48 +0800

    Using Mockito Mocks to test IndexController.

--- src/test/java/guru/framework/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/guru/framework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -1,12 +1,35 @@
 package guru.framework.spring5recipeapp.controllers;
 
+import guru.framework.spring5recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 
+    @Mock
+    RecipeService recipeService;
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
     @Test
     void getIndexPage() {
+        String expected= "index";
+        assertEquals(expected, indexController.getIndexPage(model));
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
     }
 }
--- src/test/java/guru/framework/spring5recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/guru/framework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -2,7 +2,6 @@ package guru.framework.spring5recipeapp.services;
 
 import guru.framework.spring5recipeapp.domain.Recipe;
 import guru.framework.spring5recipeapp.repositories.RecipeRepository;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;

100b8fa13ab8c87a475bd9b4a59c0897aba5e163
Sienna Hu <xh244@cornell.edu>
2021-01-13 12:01:41 +0800

    Using Mockito Mocks to test RecipeServiceImplTest

---
+++ src/test/java/guru/framework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,12 @@
+package guru.framework.spring5recipeapp.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    @Test
+    void getIndexPage() {
+    }
+}
---
+++ src/test/java/guru/framework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package guru.framework.spring5recipeapp.services;
+
+import guru.framework.spring5recipeapp.domain.Recipe;
+import guru.framework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe= new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}

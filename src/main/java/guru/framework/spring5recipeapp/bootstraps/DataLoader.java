7de80fdd6f732403f212c9152e43a0bd3426ee25
Sienna Hu <xh244@cornell.edu>
2021-01-13 11:07:40 +0800

    Using Bootstrap CSS

--- src/main/java/guru/framework/spring5recipeapp/bootstraps/DataLoader.java
+++ src/main/java/guru/framework/spring5recipeapp/bootstraps/DataLoader.java
@@ -9,6 +9,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.*;
 
@@ -27,6 +28,7 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         recipeRepository.saveAll(getRecipes());
         log.debug("Loading Bootstrap Data");

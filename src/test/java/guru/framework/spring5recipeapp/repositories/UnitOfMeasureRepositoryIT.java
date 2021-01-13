7b14cda5d675ab157956e92586aa6a49420c39fc
Sienna Hu <xh244@cornell.edu>
2021-01-13 14:36:07 +0800

    Add Maven Failsafe Plugin

---
+++ src/test/java/guru/framework/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,33 @@
+package guru.framework.spring5recipeapp.repositories;
+
+import guru.framework.spring5recipeapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", unitOfMeasureOptional.get().getDescription());
+    }
+}

341a492d4beaf1019f64df4ad1f407c41ad7965a
Sienna Hu <xh244@cornell.edu>
2021-01-14 12:13:12 +0800

    Allow Users to View a Recipe's Ingredient

---
+++ src/main/java/guru/framework/spring5recipeapp/controllers/IngredientController.java
@@ -0,0 +1,27 @@
+package guru.framework.spring5recipeapp.controllers;
+
+import guru.framework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IngredientController {
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable String recipeId, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+
+        return "recipe/ingredients/list";
+    }
+}

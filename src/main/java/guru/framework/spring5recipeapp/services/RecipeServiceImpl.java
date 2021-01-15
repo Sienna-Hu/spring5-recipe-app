c7891f7af55df72b5bdd2bd36cc3581492cfbb5b
Sienna Hu <xh244@cornell.edu>
2021-01-15 17:03:36 +0800

    Adding @ResponseStatus example

--- src/main/java/guru/framework/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/guru/framework/spring5recipeapp/services/RecipeServiceImpl.java
@@ -4,6 +4,7 @@ import guru.framework.spring5recipeapp.commands.RecipeCommand;
 import guru.framework.spring5recipeapp.converters.RecipeToRecipeCommand;
 import guru.framework.spring5recipeapp.converters.RecipeCommandToRecipe;
 import guru.framework.spring5recipeapp.domain.Recipe;
+import guru.framework.spring5recipeapp.exceptions.NotFoundException;
 import guru.framework.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -41,7 +42,7 @@ public class RecipeServiceImpl implements RecipeService{
         Optional<Recipe> recipeOptional= recipeRepository.findById(id);
 
         if (!recipeOptional.isPresent()) {
-            throw new RuntimeException("Recipe Not Found");
+            throw new NotFoundException("Recipe Not Found");
         }
 
         return  recipeOptional.get();

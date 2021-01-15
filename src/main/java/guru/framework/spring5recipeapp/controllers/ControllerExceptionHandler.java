8246639a7a02ee70813ba7a4be0b3bc8f5d6b545
Sienna Hu <xh244@cornell.edu>
2021-01-15 17:21:02 +0800

    Add ControllerAdvice

---
+++ src/main/java/guru/framework/spring5recipeapp/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package guru.framework.spring5recipeapp.controllers;
+
+import guru.framework.spring5recipeapp.exceptions.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
+        return modelAndView;
+    }
+
+}
